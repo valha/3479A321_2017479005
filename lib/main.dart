7d6d2d0e6c977f2d712df22ca56134ee064cd96c
valha <49664001+valha@users.noreply.github.com>
2024-09-04 15:14:50 -0400

    minor update
    commented a package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+//import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
